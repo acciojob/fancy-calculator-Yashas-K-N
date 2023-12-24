c445d0a8dc0609362cf11eea7dcf12960f9701a1
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2023-12-25 03:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let math = function(num1, num2){
+	let output="";
+	let res="";
+	output = 2*num1;
+	res =num2 + ouput;
+}
+let result = res;
+console.log(result);
+function();
